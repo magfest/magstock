239cab1b03127b644bfb8bc8de9a2947e75408f4
Victoria Earl <kitsuta@gmail.com>
2016-06-17 07:23:55 -0400

    Remove casting We should need to add sqlalchemy casting for a standard operation. Adding this to test on the staging server.

--- magstock/site_sections/magstock.py
+++ magstock/site_sections/magstock.py
@@ -57,7 +57,7 @@ class Root:
         campsite_assignments = []
         for site_id, site_name in c.CAMPSITE_OPTS:
             campsite_assignments.append({'site_name': site_name, 'attendees':
-                session.query(Attendee).filter(cast(site_id, sqlalchemy.String) == Attendee.site_number).all()})
+                session.query(Attendee).filter(Attendee.site_number == site_id).all()})
 
         return {
             'campsite_assignments': campsite_assignments
