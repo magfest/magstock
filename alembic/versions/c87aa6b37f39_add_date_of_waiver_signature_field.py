6a52e4c24ec8b7f8bc1a781412becf0a144a39c0
Victoria Earl <kitsuta@gmail.com>
2019-04-04 21:13:38 -0400

    Add waiver pop-up and signature requirement (#152)
    * Add waiver pop-up and signature requirement
    Fixes https://jira.magfest.net/browse/MAGDEV-430, or at least as much as possible until we get the real waiver text.
    
    * Add database migration
    It's an important file!
    
    * Remove comments in DB migration file
    
    * Add date of signature to waiver process
    Updates the waiver process according to new info in https://jira.magfest.net/browse/MAGDEV-430
    
    * Add comment explaining dual date fields

---
+++ alembic/versions/c87aa6b37f39_add_date_of_waiver_signature_field.py
@@ -0,0 +1,59 @@
+"""Add date of waiver signature field
+
+Revision ID: c87aa6b37f39
+Revises: 9fe4b5ca2ec8
+Create Date: 2019-04-04 00:02:17.870246
+
+"""
+
+
+# revision identifiers, used by Alembic.
+revision = 'c87aa6b37f39'
+down_revision = '9fe4b5ca2ec8'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+
+try:
+    is_sqlite = op.get_context().dialect.name == 'sqlite'
+except:
+    is_sqlite = False
+
+if is_sqlite:
+    op.get_context().connection.execute('PRAGMA foreign_keys=ON;')
+    utcnow_server_default = "(datetime('now', 'utc'))"
+else:
+    utcnow_server_default = "timezone('utc', current_timestamp)"
+
+def sqlite_column_reflect_listener(inspector, table, column_info):
+    """Adds parenthesis around SQLite datetime defaults for utcnow."""
+    if column_info['default'] == "datetime('now', 'utc')":
+        column_info['default'] = utcnow_server_default
+
+sqlite_reflect_kwargs = {
+    'listeners': [('column_reflect', sqlite_column_reflect_listener)]
+}
+
+# ===========================================================================
+# HOWTO: Handle alter statements in SQLite
+#
+# def upgrade():
+#     if is_sqlite:
+#         with op.batch_alter_table('table_name', reflect_kwargs=sqlite_reflect_kwargs) as batch_op:
+#             batch_op.alter_column('column_name', type_=sa.Unicode(), server_default='', nullable=False)
+#     else:
+#         op.alter_column('table_name', 'column_name', type_=sa.Unicode(), server_default='', nullable=False)
+#
+# ===========================================================================
+
+
+def upgrade():
+    op.add_column('attendee', sa.Column('waiver_date', sa.Date(), nullable=True))
+
+
+def downgrade():
+    op.drop_column('attendee', 'waiver_date')
